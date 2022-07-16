661ddad259a5a48158a8b321ccf1a90ed828af09
calt3 <coding@altmanfamily.net>
2022-07-15 21:44:35 -0400

    first commit

---
+++ test2.py
@@ -0,0 +1,15 @@
+'''def display(number):
+    if number == 2:
+        pass
+    else:
+        print("Number: ",number)
+
+display(2)
+display(3)'''
+
+list1=[1,4,"test",6,"college"]
+list2=[]
+list3=list((1,2,3))
+print(list1)
+print(list2)
+print(list3)
